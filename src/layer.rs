3448666071140d61f78a644faca6f73e3f374e18
Michael Scholten <michael-scholten@hotmail.nl>
2025-04-22 22:22:33 +0200

    Added a batch test for the layer

--- src/layer.rs
+++ src/layer.rs
@@ -49,4 +49,29 @@ mod tests {
                 .all(|(left, right)| float_equal(left, right))
         );
     }
+
+    #[test]
+    fn layer_with_batch() {
+        let inputs = [
+            [1.0, 2.0, 3.0, 2.5],
+            [2.0, 5.0, -1.0, 2.0],
+            [-1.5, 2.7, 3.3, -0.8],
+        ];
+        let layer = Layer::new([
+            Neuron::new([0.2, 0.8, -0.5, 1.0], 2.0),
+            Neuron::new([0.5, -0.91, 0.26, -0.5], 3.0),
+            Neuron::new([-0.26, -0.27, 0.17, 0.87], 0.5),
+        ]);
+        assert!(
+            layer
+                .forward_batch(inputs)
+                .flatten()
+                .zip(
+                    [[4.8, 1.21, 2.385], [8.9, -1.81, 0.2], [1.41, 1.051, 0.026]]
+                        .into_iter()
+                        .flatten()
+                )
+                .all(|(left, right)| float_equal(left, right))
+        )
+    }
 }
