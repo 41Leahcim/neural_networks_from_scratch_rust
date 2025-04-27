08b68bdac84d8656442c8709ed98ceffa283e687
Michael Scholten <michael-scholten@hotmail.nl>
2025-04-27 14:49:05 +0200

    Added a test for combining the relu and softmax functions with 2 dense layers

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,43 @@ const fn float_equal(left: f64, right: f64) -> bool {
 mod tests {
     use rand::random;
 
-    use crate::{activation::relu::ReLu, dataset::spiral, layer::Dense};
+    use crate::{
+        activation::{relu::ReLu, softmax::Softmax},
+        dataset::spiral,
+        float_equal,
+        layer::Dense,
+    };
 
     #[test]
-    fn dense_layer_with_activation() {
+    fn dense_layer_with_relu() {
         let (x, _) = spiral(100, 3);
         let dense = random::<Dense<2, 3>>();
         let mut output = ReLu.forward_batch(dense.forward_batch(x));
         assert!(output.next().unwrap().into_iter().all(|value| value == 0.0));
         assert!(output.flatten().all(|value| value >= 0.0));
+    }
+
+    #[test]
+    fn dense_layer_with_activation_functions() {
+        // Create dataset
+        let (x, _) = spiral(2, 3);
+
+        // Create a dense layer
+        let dense1 = random::<Dense<2, 3>>();
+
+        // Create a ReLU activation layer
+        let activation1 = ReLu;
+
+        // Create the second dense layer with 3 input features (as it takes the output of the
+        // previous layer here) and 3 output values
+        let dense2 = random::<Dense<3, 3>>();
+
+        // Create the Softmax activation (to be used with dense2)
+        let activation2 = Softmax;
+        let output = dense1.forward_batch(x);
+        let output = activation1.forward_batch(output);
+        let output = dense2.forward_batch(output);
+        let output = activation2.forward_batch(output);
+        assert!(output.flatten().all(|value| float_equal(value, 1.0 / 3.0)));
     }
 }
